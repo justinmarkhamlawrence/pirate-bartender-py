2b79221e0e713a31228850201774ea30b42a435a
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-10-10 17:13:06 -0400

    initial commit

---
+++ pirate-bartender.py
@@ -0,0 +1,29 @@
+import sys
+
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?",
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"],
+}
+
+answers = {
+    "strong": False,
+    "salty": False,
+    "bitter": False,
+    "sweet": False,
+    "fruity": False
+}
+
+for k,v in questions.items():
+    my_input = input(v)
+    print(my_input)
